6e2a668c1e0cda9fa7386cc85e34c02c3660cb5f
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-10-21 13:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let p = document.createElement('p')
+p.setAttribute('id',"timer")
+
+setInterval(()=>{
+	let date = new Date()
+	p.textContent =`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}, ${date.toLocaleTimeString("en-US")}`
+	document.body.appendChild(p)
+},1000)
